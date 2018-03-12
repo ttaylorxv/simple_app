78a41c70e2bb48a407df515b9fe95855052cbaa2
Tonye <ttaylor@crossvale.com>
2018-03-12 11:51:03 -0500

    configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   #for deploying to Openshift
-  config.secret_key_base = ENV["SECRET_KEY_BASE"] 
+  #config.secret_key_base = ENV["SECRET_KEY_BASE"] 
 end
